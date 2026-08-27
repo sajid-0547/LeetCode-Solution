75cbd7d8372454757b7235ec79d2f683ab6b8ba6
Mohammad Sajid <mohammadsajid0547@gmail.com>
2026-08-28 02:36:22 +0530

    Time: 2077 ms (61.44%) | Memory: 42.3 MB (26.04%) - LeetSync

---
+++ 507-perfect-number/perfect-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static  int check(int num){
+        int sum = 0;
+        for(int i=1;i<=num/2;i++){
+            if(num%i == 0){
+                sum += i;
+            }
+        }
+        return sum;
+    }
+    public boolean checkPerfectNumber(int num) {
+        int a = check(num);
+        if( a == num) return true;
+
+       return false; 
+        
+    }
+}
