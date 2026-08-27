518e21c65ddcef500618a5360f4891275cebd087
Mohammad Sajid <mohammadsajid0547@gmail.com>
2026-08-27 20:04:49 +0530

    Time: 1 ms (89.82%) | Memory: 42.5 MB (70.98%) - LeetSync

---
+++ 342-power-of-four/power-of-four.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        for(int i=0;i<31;i++){
+            if(n == Math.pow(4,i)) return true;
+        }
+        return false;
+        
+    }
+}
