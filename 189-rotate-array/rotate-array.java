d0ad5410bb63d3f8135c7a4f8530289ec147f34e
Mohammad Sajid <mohammadsajid0547@gmail.com>
2026-08-30 19:26:21 +0530

    Time: 4 ms (24.44%) | Memory: 268.6 MB (15.18%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,23 +1,42 @@
 class Solution {
-    public void rotate(int[] nums, int k) {
-        k=k%nums.length;
-        int index = nums.length-k;
-        int[] arr = new int[nums.length];
-        int i = 0;
-        while(i != k){
-            arr[i] = nums[index];
-            index++;
-            i++;
-        }
-        for(int j=0;j<nums.length-k;j++){
-            arr[i] = nums[j];
-            i++;
-        }
-        for(int l=0;l<nums.length;l++){
-            nums[l] = arr[l];
-        }
+    void reverse(int[] nums,int start,int end) {
+        while(start<end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
 
-        
+            start++;
+            end--;
+        }
+    }
+    public void rotate(int[] nums, int k) {
+
+        //addtional array
+
+        // k=k%nums.length;
+        // int index = nums.length-k;
+        // int[] arr = new int[nums.length];
+        // int i = 0;
+        // while(i != k){
+        //     arr[i] = nums[index];
+        //     index++;
+        //     i++;
+        // }
+        // for(int j=0;j<nums.length-k;j++){
+        //     arr[i] = nums[j];
+        //     i++;
+        // }
+        // for(int l=0;l<nums.length;l++){
+        //     nums[l] = arr[l];
+        // }
+
+        // two pointer way
+
+        k = k%nums.length;
+
+        reverse(nums,0,nums.length-1); 
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
         
     }
 }
