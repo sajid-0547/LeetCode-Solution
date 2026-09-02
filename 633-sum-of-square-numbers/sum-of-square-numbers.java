6bffaadbe6d7b995486934e473f9dc6a2202b0d9
Mohammad Sajid <mohammadsajid0547@gmail.com>
2026-09-03 00:53:16 +0530

    Time: 4 ms (82.77%) | Memory: 42.4 MB (15.52%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        long i = 0;
+        long j = (int)Math.sqrt(c);
+
+        while(i<=j){
+            if((i*i+j*j) == c){
+                return true;
+            }else if((i*i+j*j)>c){
+                j--;
+            }else{
+                i++;
+            }
+        }
+        return false;
+    }
+}
