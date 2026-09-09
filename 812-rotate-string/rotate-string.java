546372acb64bf8225a7f07d126756c5e3e9441d6
Mohammad Sajid <mohammadsajid0547@gmail.com>
2026-09-09 20:37:20 +0530

    Time: 3 ms (6.48%) | Memory: 43.7 MB (15.58%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+
+        if(s.length() != goal.length()) return false;
+
+        int i=0;
+        while(i < s.length()){
+            goal = goal.charAt(goal.length()-1)+goal.substring(0,goal.length()-1);
+            if(goal.equals(s)) return true;
+            i++;
+        }
+
+
+        return false;
+        
+    }
+}
