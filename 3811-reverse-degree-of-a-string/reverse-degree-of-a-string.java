0200fd589d080c8d55b93542fe64a16705efafc0
Mohammad Sajid <mohammadsajid0547@gmail.com>
2026-09-21 03:20:39 +0530

    Time: 1 ms (100.00%) | Memory: 44 MB (67.14%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reverseDegree(String s) {
+     int sum = 0;
+
+    for(int i = 0; i < s.length(); i++){
+        char ch = s.charAt(i);
+        int value = 'z' - ch + 1;
+        int temp = i+1;
+        sum = sum + value*temp;
+    }
+
+    return sum;
+    }
+}
